df288904d5421684b20ea98fa61def6722f3b2ab
Mahmoud_Gad <hooda.gad333@gmail.com>
2024-05-25 15:15:17 +0300

    do a small change in time.js file

--- js/time.js
+++ js/time.js
@@ -4,24 +4,25 @@ const frmt_time = (n) => {
   return Math.floor(Math.log10(n)) ? n : "0" + n;
 };
 
-const hours = document.querySelector(".clock > .time > .hr");
-const minutes = document.querySelector(".clock > .time > .min");
-const seconds = document.querySelector(".clock > .time > .sec");
+{
+  const hours = document.querySelector(".clock > .time > .hr");
+  const minutes = document.querySelector(".clock > .time > .min");
+  const seconds = document.querySelector(".clock > .time > .sec");
 
-setInterval(() => {
-  const D = new Date();
-  const hrs = frmt_time(D.getHours());
-  const mins = frmt_time(D.getMinutes());
-  const secs = frmt_time(D.getSeconds());
+  setInterval(() => {
+    const D = new Date();
+    const hrs = frmt_time(D.getHours());
+    const mins = frmt_time(D.getMinutes());
+    const secs = frmt_time(D.getSeconds());
 
-  hours.textContent = hrs;
-  minutes.textContent = mins;
-  seconds.textContent = secs;
-}, 1000);
+    hours.textContent = hrs;
+    minutes.textContent = mins;
+    seconds.textContent = secs;
+  }, 1000);
+}
 /* Time End */
 
 /* Date Start */
-
 {
   const months = [
     "January",
