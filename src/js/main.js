57e7c8e294e609d5b1c6c9fcde4b1e35eab208fb
Mahmoud_Gad <hooda.gad333@gmail.com>
2025-01-19 07:23:43 +0200

    fix live error

--- src/js/main.js
+++ src/js/main.js
@@ -779,9 +779,9 @@ set_lang();
       const loadingPage = document.getElementById("loadingPage");
       // get data from api
       const all = await Promise.allSettled([
-        fetch("https://eg-prices-api.vercel.app/gold"),
-        fetch("https://eg-prices-api.vercel.app/silver"),
-        fetch("https://eg-prices-api.vercel.app/prices"),
+        fetch("https://eg-prices-api-old.vercel.app/gold"),
+        fetch("https://eg-prices-api-old.vercel.app/silver"),
+        fetch("https://eg-prices-api-old.vercel.app/prices"),
       ]);
 
       const res1 = await all[0].value,
@@ -1011,7 +1011,7 @@ async function play_live() {
     }
   };
 
-  const url = "https://eg-prices-api.vercel.app/live";
+  const url = "https://eg-prices-api-old.vercel.app/live";
 
   // init
   const res = await fetch(url);
